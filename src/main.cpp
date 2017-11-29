a1dc97d01751122fec02de3a279f78b1c59e8f60
ItsPax <alexsieu14@gmail.com>
2017-11-29 15:41:06 -0700

    little oop magic

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-#include <Arduino.h>
 
+#include "frontier.h" 
+
+int main()  {
+    Frontier io;
+    io.fillFrontier();
+    io.printFrontier();
+    io.spawnBlock();
+    int i = 0;
+    while (i < 2)    {
+        io.turn();
+        io.printFrontier();
+        ++i;
+    }
+    return 0;
+}
 
